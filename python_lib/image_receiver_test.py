07c980864bb4edec0ab59215d1d7ee0de6855e5c
vas59 <vasily.yuryev.a1@gmail.com>
2019-08-14 17:51:11 +0300

    python receiver work

--- python_lib/image_receiver_test.py
+++ python_lib/image_receiver_test.py
@@ -1,14 +1,14 @@
 import cv2
 from opencv_image_transfer.ImgReceiver import Receiver
 
-recv = Receiver("udp://127.0.0.1:1853")
+recv = Receiver("tcp://127.0.0.1:1258")
 
 recv.open()
 
 while cv2.waitKey(1) != ord('q'):
     ret, image = recv.read()
-    if ret == True:
-        cv2.imshow("recv", image)
+    # print(ret)
+    cv2.imshow("recv", image)
 
 recv.close()
 cv2.destroyAllWindows()
--- python_lib/opencv_image_transfer/ImgReceiver.py
+++ python_lib/opencv_image_transfer/ImgReceiver.py
@@ -2,9 +2,10 @@ import cv2
 import threading
 import time
 import numpy as np
+import zmq
 
 class Receiver:
-    def __init__(self, url, image_size = (240, 320, 3), timeout = 0.5):
+    def __init__(self, url, image_size = (240, 320, 3), timeout = 2):
         self.running = False
         self.image_size = image_size
         self.timeout = timeout
@@ -13,17 +14,28 @@ class Receiver:
         self.url = url
         self.t = threading.Thread(target=self.__receiver_thread)
         self.t.daemon = True
-        self.t.start()
+        self.l_t = -1
 
     def __receiver_thread(self):
+        self.l_t = -1
         while self.running:
-            pass
+            image_bytes = self.receiver.recv()
+            self.image = np.frombuffer(image_bytes, dtype='uint8').reshape(self.image_size)
+            self.l_t = time.time()
+
     def open(self):
         self.running = True
+        self.context = zmq.Context()
+        self.receiver = self.context.socket(zmq.SUB)
+        self.receiver.connect(self.url)
+        self.receiver.setsockopt_string(zmq.SUBSCRIBE, "")
+
+        self.t.start()
     def close(self):
         self.running = False
         # wait
         self.t.join()
     def read(self):
         # return 
+        self.timeouted = (time.time() - self.l_t) > self.timeout
         return (self.running and (not self.timeouted)), self.image
